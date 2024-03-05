a0de3e383bd9bbdaf6ec15135429ab4f0932419b
Azurhys <123641752+Azurhys@users.noreply.github.com>
2024-03-05 11:44:02 +0100

    Serveur conteneur + docker file

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 9000;
+
+
+const db = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'example',
+    database: 'JO'
+});
+
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connecté à la base de données MySQL');
+});
+
+
+app.get('/sports', (req, res) => {
+    db.query('SELECT * FROM Sports', (err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Erreur lors de la récupération des sports' });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.get('/medailles', (req, res) => {
+    db.query('SELECT * FROM Medailles', (err, result) => {
+        if(err) {
+            res.status(500).json({ error: 'Erreur lors de la récupération des données'})
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+app.get('/epreuves', (req, res) => {
+    db.query('SELECT * FROM Epreuves', (err, result) => {
+        if(err) {
+            res.status(500).json({ error: 'Erreur lors de la récupération des données'})
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
